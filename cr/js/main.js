97465998d36032ee4655bb991fb8b3b811b24124
Liam Naddell <beanboybunny@gmail.com>
2016-02-19 20:18:40 -0500

    save

--- cr/js/main.js
+++ cr/js/main.js
@@ -1,1 +1,20 @@
-console.log("Im wurking);
+var c = {
+  LukeC : [
+    "Luke C",
+    "LC",
+  ],
+  ParkerS : [
+    "Parker S",
+    "PS",
+  ],
+  WalkerS : [
+    "Walker S",
+    "WS",
+  ],
+  LiamN : [
+    "Liam N",
+    "LN",
+  ],
+}
+console.log("Im wurking");
+console.log(c.LukeC[0])
