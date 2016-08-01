ca58b535b6c80d19b5a870fed98742c9135371e6
Liam Naddell <beanboybunny@gmail.com>
2016-08-01 11:38:56 -0400

    save

--- fb/js/about.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', startup);
-
-function startup() {
-  console.log("im working");
-}
--- fb/js/home.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', initilize);
-
-function initilize() {
-  console.log("I am working");
-}
